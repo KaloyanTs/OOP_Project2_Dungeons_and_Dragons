166d8c535d9432317a8fca55ff9dac9998cb503c
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-05-26 14:09:51 +0300

    testing printer

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
-
+#include "Image.hpp"
+#include "Printer.hpp"
 
 int main()
 {
-
+    Image logo("assets\\logo.dnd");
+    Printer out;
+    out(logo);
+    std::cin.get();
     return 0;
 }
