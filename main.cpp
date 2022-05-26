f07a6941ad5ba627024a09638743896befca8311
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-05-27 01:29:08 +0300

    testing mImPrinter

--- main.cpp
+++ main.cpp
@@ -5,13 +5,25 @@
 int main()
 {
     Image logo("assets\\logo.dnd");
-    Image dragon7("assets\\dragon7.dnd");
-    Image dragon8("assets\\dragon8.dnd");
-    Image *dragons[] = {&dragon7, &dragon8};
+    Image dragon1("assets\\dragon7.dnd");
+    Image dragon2("assets\\dragon8.dnd");
+    Image *dragons[] = {&dragon1, &dragon2};
     Printer out;
     out(logo);
     MultipleImagePrinter mOut;
     mOut(dragons, 2, 10);
     std::cin.get();
+    Image heartArr[] =
+        {
+            Image("assets\\health.dnd"),
+            Image("assets\\spell.dnd"),
+            Image("assets\\health.dnd")};
+    Image *hearts[] =
+        {
+            &heartArr[0],
+            &heartArr[1],
+            &heartArr[2]};
+
+    mOut(hearts, 3, 5);
     return 0;
 }
