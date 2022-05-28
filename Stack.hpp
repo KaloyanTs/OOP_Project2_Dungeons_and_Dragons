d4f8a93179e00938c4a5ac4f745311754fba6208
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-05-28 11:19:36 +0300

    stack implemented

--- Stack.hpp
+++ Stack.hpp
@@ -7,22 +7,32 @@ template <typename T>
 class Stack : protected Vector<T>
 {
 public:
-    T top()const
-    {
-        //todo in cpp
-        assert(size);
-        return data[size - 1];
-    }
-    Stack &push(const T &el)
-    {
-        //todo in cpp
-        push_back(el);
-        return *this;
-    }
-    void pop()
-    {
-        //todo in cpp
-    }
+    T top() const;
+    Stack &push(const T &el);
+    void pop();
+    bool empty() const { return !Vector<T>::size; }
 };
+
+template <typename T>
+T Stack<T>::top() const
+{
+    assert(Vector<T>::size);
+    return Vector<T>::data[Vector<T>::size - 1];
+}
+
+template <typename T>
+Stack<T> &Stack<T>::push(const T &el)
+{
+    Vector<T>::push_back(el);
+    return *this;
+}
+
+template <typename T>
+void Stack<T>::pop()
+{
+    --Vector<T>::size;
+    if (Vector<T>::capacity > Vector<T>::INIT_CAPACITY && (Vector<T>::size << 1) <= Vector<T>::capacity)
+        Vector<T>::shrink();
+}
 
 #endif
