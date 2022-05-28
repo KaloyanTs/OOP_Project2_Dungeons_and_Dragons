beda3459d0b914fca3835a2897c9c53acf16d7b6
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-05-28 14:06:41 +0300

    change of names

--- Stack.hpp
+++ Stack.hpp
@@ -10,14 +10,14 @@ public:
     T top() const;
     Stack &push(const T &el);
     void pop();
-    bool empty() const { return !Vector<T>::size; }
+    bool empty() const { return !Vector<T>::m_size; }
 };
 
 template <typename T>
 T Stack<T>::top() const
 {
-    assert(Vector<T>::size);
-    return Vector<T>::data[Vector<T>::size - 1];
+    assert(Vector<T>::m_size);
+    return Vector<T>::data[Vector<T>::m_size - 1];
 }
 
 template <typename T>
@@ -30,8 +30,9 @@ Stack<T> &Stack<T>::push(const T &el)
 template <typename T>
 void Stack<T>::pop()
 {
-    --Vector<T>::size;
-    if (Vector<T>::capacity > Vector<T>::INIT_CAPACITY && (Vector<T>::size << 1) <= Vector<T>::capacity)
+    --Vector<T>::m_size;
+    if (Vector<T>::m_capacity > Vector<T>::INIT_CAPACITY &&
+        (Vector<T>::m_size << 1) <= Vector<T>::m_capacity)
         Vector<T>::shrink();
 }
 
--- Vector.hpp
+++ Vector.hpp
@@ -1,6 +1,7 @@
 #ifndef __VECTOR_HPP
 #define __VECTOR_HPP
 #include <cstddef>
+#include <cassert>
 
 template <typename T>
 class Vector
@@ -8,29 +9,32 @@ class Vector
 protected:
     static const unsigned INIT_CAPACITY = 2;
     T *data;
-    size_t size, capacity;
+    size_t m_size, m_capacity;
 
     void expand();
     void shrink();
     void copy(const Vector &other);
 
 public:
-    Vector(size_t cap = INIT_CAPACITY) : data(new T[cap]), capacity(cap), size(0) {}
+    Vector(size_t cap = INIT_CAPACITY) : data(new T[cap]), m_capacity(cap), m_size(0) {}
     Vector(const Vector &other);
     Vector &operator=(const Vector &other);
     ~Vector();
 
-    unsigned getCount() const { return size; }
+    unsigned getCount() const { return m_size; }
     Vector &push_back(const T &el);
-    bool empty() const { return !size; }
+    size_t size() const { return m_size; }
+    size_t capacity() const { return m_capacity; }
+    bool empty() const { return !m_size; }
+    const T &operator[](unsigned index) const;
 };
 
 template <typename T>
 void Vector<T>::expand()
 {
-    capacity <<= 1;
-    T *newData = new T[capacity];
-    for (unsigned i = 0; i < size; ++i)
+    m_capacity <<= 1;
+    T *newData = new T[m_capacity];
+    for (unsigned i = 0; i < m_size; ++i)
         newData[i] = data[i];
     delete[] data;
     data = newData;
@@ -39,9 +43,9 @@ void Vector<T>::expand()
 template <typename T>
 void Vector<T>::shrink()
 {
-    capacity >>= 1;
-    T *newData = new T[capacity];
-    for (unsigned i = 0; i < size; ++i)
+    m_capacity >>= 1;
+    T *newData = new T[m_capacity];
+    for (unsigned i = 0; i < m_size; ++i)
         newData[i] = data[i];
     delete[] data;
     data = newData;
@@ -51,16 +55,16 @@ template <typename T>
 void Vector<T>::copy(const Vector &other)
 {
     delete[] data;
-    size = other.size;
-    capacity = other.capacity;
-    data = new T[capacity];
-    for (unsigned i = 0; i < size; ++i)
+    m_size = other.m_size;
+    m_capacity = other.m_capacity;
+    data = new T[m_capacity];
+    for (unsigned i = 0; i < m_size; ++i)
         data[i] = other.data[i];
 }
 
 template <typename T>
 Vector<T>::Vector(const Vector &other)
-    : data(nullptr), size(0)
+    : data(nullptr), m_size(0)
 {
     copy(other);
 }
@@ -82,10 +86,17 @@ Vector<T>::~Vector()
 template <typename T>
 Vector<T> &Vector<T>::push_back(const T &el)
 {
-    if (size == capacity)
+    if (m_size == m_capacity)
         expand();
-    data[size++] = el;
+    data[m_size++] = el;
     return *this;
+}
+
+template <typename T>
+const T &Vector<T>::operator[](unsigned index) const
+{
+    assert(index < m_size);
+    return data[index];
 }
 
 #endif
