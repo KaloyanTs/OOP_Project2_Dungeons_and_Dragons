bff7cc11c7e8fa09415cc9c65d3cfff48e1705c5
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-05-28 14:44:21 +0300

    boolean telling if this is currently on board

--- EventGenerator.hpp
+++ EventGenerator.hpp
@@ -5,12 +5,16 @@
 class EventGenerator
 {
     unsigned posY, posX;
+    bool onBoard = 0;
 
 public:
-    EventGenerator(unsigned y, unsigned x) : posY(y), posX(x) {}
+    EventGenerator(unsigned y, unsigned x, unsigned board = true)
+        : posY(y), posX(x), onBoard(board) {}
     virtual char getChar() const = 0;
     virtual void print(const Printer &p) const = 0;
     bool locatedAt(unsigned y, unsigned x) { return posY == y && posX == x; }
+
+    bool isOnBoard() const { return onBoard; }
 };
 
 #endif
