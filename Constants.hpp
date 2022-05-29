3fc435961dedb6f30be99eb944683c9f4211466e
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-05-29 12:38:45 +0300

    more constants

--- Constants.hpp
+++ Constants.hpp
@@ -6,11 +6,22 @@ class Constants
     Constants() {}
 
 public:
+    static const unsigned HERO_LEVELUP_POINTS = 30;
+    static const unsigned WARRIOR_INIT_ATTACK = 40;
+    static const unsigned WARRIOR_INIT_MANA = 10;
+    static const unsigned WARRIOR_INIT_HEALTH = 50;
+    static const unsigned MAGE_INIT_ATTACK = 10;
+    static const unsigned MAGE_INIT_MANA = 40;
+    static const unsigned MAGE_INIT_HEALTH = 50;
+    static const unsigned HUMAN_INIT_ATTACK = 30;
+    static const unsigned HUMAN_INIT_MANA = 20;
+    static const unsigned HUMAN_INIT_HEALTH = 50;
     static const unsigned MONSTER_INIT_ATTACK = 25;
     static const unsigned MONSTER_INIT_MANA = 25;
     static const unsigned MONSTER_INIT_HEALTH = 50;
     static const unsigned MONSTER_COUNT_1 = 2;
     static const unsigned MONSTER_COUNT_2 = 3;
+    static const unsigned DRAGON_SCALE_ARMOR = 15;
     static const unsigned TREASURE_COUNT_1 = 2;
     static const unsigned TREASURE_COUNT_2 = 2;
 };
