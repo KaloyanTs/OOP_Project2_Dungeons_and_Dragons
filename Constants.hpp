e93c4ff15c62cb02ab9a049484c015db7edc23e9
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-05-29 12:05:46 +0300

    holder for constants

---
+++ Constants.hpp
@@ -0,0 +1,18 @@
+#ifndef __CONSTANTS_HPP
+#define __CONSTANTS_HPP
+
+class Constants
+{
+    Constants() {}
+
+public:
+    static const unsigned MONSTER_INIT_ATTACK = 25;
+    static const unsigned MONSTER_INIT_MANA = 25;
+    static const unsigned MONSTER_INIT_HEALTH = 50;
+    static const unsigned MONSTER_COUNT_1 = 2;
+    static const unsigned MONSTER_COUNT_2 = 3;
+    static const unsigned TREASURE_COUNT_1 = 2;
+    static const unsigned TREASURE_COUNT_2 = 2;
+};
+
+#endif
