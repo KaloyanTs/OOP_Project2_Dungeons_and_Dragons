8c916791f2df82e6bb90c32a9390b9f69a68910a
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-05-30 21:18:40 +0300

    new assets

--- GameAssets.cpp
+++ GameAssets.cpp
@@ -4,5 +4,8 @@ const Image GameAssets::health_icon("assets\\health.dnd");
 const Image GameAssets::attack_icon("assets\\weapon.dnd");
 const Image GameAssets::magic_icon("assets\\spell.dnd");
 const Image GameAssets::dragon("assets\\dragon.dnd");
+const Image GameAssets::weapon_icon("assets\\weapon.dnd");
+const Image GameAssets::armor_icon("assets\\armor.dnd");
+const Image GameAssets::spell_icon("assets\\spell.dnd");
 const Image *GameAssets::stats_header[] = {
     &health_icon, &attack_icon, &magic_icon};
--- GameAssets.hpp
+++ GameAssets.hpp
@@ -11,8 +11,11 @@ public:
     static const Image health_icon;
     static const Image attack_icon;
     static const Image magic_icon;
+    static const Image weapon_icon;
+    static const Image armor_icon;
+    static const Image spell_icon;
     static const Image dragon;
-    static const size_t stats_count=3;
+    static const size_t stats_count = 3;
     static const Image *stats_header[stats_count];
 };
 
