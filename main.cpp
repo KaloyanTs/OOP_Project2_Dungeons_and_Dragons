96c6f751f6118157c79f88526f4cc6730e05df96
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-05-31 10:51:50 +0300

    testing input of hero

--- main.cpp
+++ main.cpp
@@ -13,9 +13,18 @@ int main()
     out(logo);
     out("\tPress any key to play...");
     getch();
-    // std::cin.get();
+    system("cls");
+    std::cout << "Choose a hero:\n\t1 for human\n\t2 for mage\n\t3 for warrior\n";
+    char chosen;
+    do
+    {
+        chosen = getch();
+    } while (chosen - '0' >= Constants::HERO_TYPES || chosen - '0' < 0);
+    char name[100]; // fix use String
+    std::cout << "Choose name for your hero:\t";
+    std::cin.getline(name, 100);
     MultipleImagePrinter p;
-    Map m(p, 4);
+    Map m(p, Map::getHero(chosen - '0', name), 4);
     // fix Map m(p, "assets\\level4.dndmap");
 
     m.run();
