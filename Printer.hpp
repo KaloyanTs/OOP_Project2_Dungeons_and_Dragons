61417f6a05cfafb0eefdbefc78a3cb2dd46bab56
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-06-03 15:03:41 +0300

    precision of the output

--- Printer.hpp
+++ Printer.hpp
@@ -1,6 +1,7 @@
 #ifndef __Printer_HPP
 #define __Printer_HPP
 #include <iostream>
+#include <iomanip>
 #include "GameAssets.hpp"
 
 class Printer
@@ -11,7 +12,7 @@ protected:
     std::ostream &getOutputStream() const { return os; }
 
 public:
-    Printer(std::ostream &_os = std::cout) : os(_os) {}
+    Printer(std::ostream &_os = std::cout) : os(_os) { os.precision(2); }
     template <typename Printable>
     const Printer &operator()(const Printable &p) const
     {
