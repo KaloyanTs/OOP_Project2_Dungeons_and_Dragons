1bd58c58be09fd1efe931d38b1bc2221cb0cafea
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-06-08 19:22:38 +0300

    custom exception

---
+++ MyException.hpp
@@ -0,0 +1,17 @@
+#ifndef __MY_EXCEPTION_HPP
+#define __MY_EXCEPTION_HPP
+#include <exception>
+#include "String.hpp"
+
+class MyException : public std::exception
+{
+    String errorMsg;
+    String foundIn;
+
+public:
+    MyException(const String &err, const String &in = "") : errorMsg(err), foundIn(in) {}
+    const char *what() const noexcept override { return errorMsg.c_str(); }
+    const char *where() const { return foundIn.c_str(); }
+};
+
+#endif
